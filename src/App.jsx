f118335931b71226b9b9faf3820a55cc19210233
SugandhKumars <merrimarrzi@gmail.com>
2024-06-13 10:39:22 +0530

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div>Traffic Light</div>
+    </>
+  );
+}
+function Pole() {
+  return (
+    <>
+      <div>Pole</div>
+    </>
+  );
+}
+export default App;
